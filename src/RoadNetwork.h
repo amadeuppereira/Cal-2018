e5d0a3fb8a78254e9e282a686f330a68572fda12
Amadeu Pereira <up201605646@fe.up.pt>
2018-04-10 23:14:16 +0100

    added comments

--- src/RoadNetwork.h
+++ src/RoadNetwork.h
@@ -20,35 +20,133 @@
 #define HIGH_TRAFFIC "red"
 #define BLOCKED_EDGE_COLOR "black"
 
-
+/**
+ * Classe que guarda todas as informacoes necessarias, que faz todos os calculos
+ * necessarios com essas informacoes e atualiza o graphviewer
+ */
 class RoadNetwork {
 private:
+	/**
+	 * Variavel que guarda o grafo
+	 */
 	Graph<int> graph;
+	/**
+	 * Variavel que guarda o graphviewer
+	 */
 	GraphViewer *gv;
 
 public:
+	/**
+	 * Construtor
+	 */
 	RoadNetwork();
+	/**
+	 * Destrutor
+	 */
 	virtual ~RoadNetwork();
+	/**
+	 * Le os ficheiros com informacao relativa aos vertices, as edges e aos carros
+	 * e atualiza o grafo com essa informacao
+	 */
 	void readOSM();
+	/**
+	 * Escreve a informacao das edges no ficheiro correspondente
+	 */
 	void writeEdgeFile();
+	/**
+	 * Converte a informacao contida no grafo numa do tipo graphviewer
+	 */
 	void convertToGV();
+	/**
+	 * Devolve o grafo
+	 * @return grafo
+	 */
 	const Graph<int>& getGraph() const;
+	/**
+	 * Indica se uma estrada se encontra bloqueada ou nao
+	 * @param name nome da estrada que se pretende verificar
+	 * @return true se estiver bloqueada, false se nao
+	 */
 	bool getEdgeBlockedStatus(string name);
+	/**
+	 * Altera o estado de bloqueada, ou nao, de uma estrada
+	 * @param edge_name nome da estrada que se pretende alterar
+	 * @param blocked novo valor
+	 */
 	void setEdgeBlocked(string edge_name, bool blocked);
+	/**
+	 * Devolve o peso total de um percurso
+	 * @param vec vetor com os vertices do percurso a calcular
+	 * @return valor do peso do percurso
+	 */
 	double getWeightOfPath(vector<Vertex<int>*> vec);
+	/**
+	 * Imprime a informacao relativa a um percurso, atualizando, tambem, o graphviewer
+	 * com. Adiciona um novo carro ao programa cada vez que é executada.
+	 * @param nodeStartID
+	 * @param nodeDestinationID
+	 */
 	void printPath(int nodeStartID, int nodeDestinationID);
+	/**
+	 * Imprime o percurso completo de todos os carros
+	 */
 	void printAllCarPath() const;
+	/**
+	 * Imprime o inicio e o fim do percurso de todos os carros
+	 */
 	void printCarID() const;
+	/**
+	 * Remove um carro do programa
+	 * @param id id do carro a remover
+	 * @return true so foi possivel remover, falso se nao
+	 */
 	bool removeCar(int id);
+	/**
+	 * Escreve a informacao relativa a todos os carros no ficheiro
+	 */
 	void writeCarsFile();
+	/**
+	 * Atualiza a cor de um vertice no graphviewer para amarelo (percurso)
+	 * @param id id do vertice a atualizar
+	 */
 	void highlightNode(int id) const;
+	/**
+	* Atualiza a cor de uma edge no graphviewer para amarelo (percurso)
+	* @param id id da edge a atualizar
+	*/
 	void highlightEdge(int id) const;
+	/**
+	 * Atualiza a cor de um vertice para a sua cor padrao
+	 * @param id id do vertice a atualizar
+	 */
 	void removeHighlightNode(int id) const;
+	/**
+	 * Atualiza a cor de uma edge para a sua cor padrao
+	 * @param id id da edge a atualizar
+	 */
 	void removeHighlightEdge(int id) const;
+	/**
+	 * Atualiza a cor de uma edge para a preto (bloqueada)
+	 * @param id id da edge a atualizar
+	 */
 	void blockEdge(int id) const;
+	/**
+	 * Atualiza a cor de uma edge para a sua cor padrao
+	 * @param id id da edge a atualizar
+	 */
 	void removeBlockEdge(int id) const;
+	/**
+	 * Atualiza a cor das edges tendo em conta o numero de carros que por ela circulam
+	 * e se se encontra bloqueada ou nao
+	 */
 	void updateMap() const;
+	/**
+	 * Fecha a janela do graphviewer
+	 */
 	void closeMapWindow() const;
+	/**
+	 * Atualiza todo o conteudo do grafo
+	 */
 	void updateInfo();
 };
 
--- src/Utils.h
+++ src/Utils.h
@@ -14,10 +14,30 @@
 #define MIN_LAT 36.8554
 #define MAX_LAT 41.9411
 
-
+/**
+ * Classe que guarda informacao relativa as relacoes entres as edges e os
+ * vertices iniciais e finais das mesas
+ */
 class Link {
 public:
-	int edgeID, nodeID1, nodeID2;
+	/**
+	 * Variavel que guarda o id da edge
+	 */
+	int edgeID;
+	/**
+	 * Variavel que guarda o id do vertice inicial da edge
+	 */
+	int nodeID1;
+	/**
+	 * Variavel que guarda o id do vertice final da edge
+	 */
+	int nodeID2;
+	/**
+	 * Construtor da class Link
+	 * @param e id da edge
+	 * @param n1 id do vertice inicial
+	 * @param n2 id do vertice final
+	 */
 	Link(int e, int n1, int n2) {
 		edgeID = e;
 		nodeID1 = n1;
@@ -25,8 +45,20 @@ public:
 	}
 };
 
-
+/**
+ * Calcula a posicao relativa, no mapa, da latitude dada, tendo por base o tamanho
+ * do mapa e as suas coordenadas extremas
+ * @param lat latitude
+ * @return nova posicao
+ */
 int resizeLat(long double lat);
+
+/**
+ * Calcula a posicao relativa, no mapa, da longitude dada, tendo por base o tamanho
+ * do mapa e as suas coordenadas extremas
+ * @param lon longitude
+ * @return nova posicao
+ */
 int resizeLon(long double lon);
 
 
